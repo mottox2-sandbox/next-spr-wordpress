623091270e8616e652fed798100745e5f0263f92
mottox2 <y.mottox2@gmail.com>
2019-04-26 02:00:23 +0900

    Add styles

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,68 @@ import React from 'react'
 import axios from 'axios'
 import Link from 'next/link'
 
+import { jsx, css, Global } from '@emotion/core'
+import {
+  Layer,
+  Toolbar,
+  Text,
+  Navbar,
+  DarkMode,
+  Tabs,
+  Tab,
+  List,
+  ListItem,
+  IconChevronRight
+} from 'sancho'
+
 const Index = props => {
   return (
     <>
-      {props.posts.map(post => {
-        return (
-          <div key={post.id}>
-            <Link href={`/post?id=${post.id}`}>{post.title.rendered}</Link>
-          </div>
-        )
-      })}
+      <Global
+        styles={{
+          body: {
+            padding: 0,
+            margin: 0
+          }
+        }}
+      />
+      <Toolbar
+        style={{
+          backgroundColor: 'rgb(52, 58, 64)'
+        }}
+      >
+        <DarkMode>
+          <Text varient="h5">Demo Blog with WP REST API</Text>
+        </DarkMode>
+      </Toolbar>
+      <DarkMode>
+        <Tabs
+          css={css`
+            background-color: rgb(52, 58, 64);
+          `}
+        >
+          <Tab>All</Tab>
+          <Tab>Develop</Tab>
+          <Tab>Design</Tab>
+          <Tab>Event</Tab>
+        </Tabs>
+      </DarkMode>
+      <List>
+        {props.posts.map(post => {
+          console.log(post.id)
+          return (
+            <Link href={`/post?id=${post.id}`} key={post.id}>
+              <ListItem
+                wrap={false}
+                primary={post.title.rendered}
+                secondary={post.excerpt.rendered}
+                key={post.id}
+                contentAfter={<IconChevronRight />}
+              />
+            </Link>
+          )
+        })}
+      </List>
     </>
   )
 }
